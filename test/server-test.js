1aee327fd856a48a83a38f4330e28d478431dcc8
bkazic <blaz.kazic@ijs.si>
2015-10-03 14:11:07 +0200

    code cleanup

--- test/server-test.js
+++ test/server-test.js
@@ -1,14 +1,9 @@
 ﻿var assert = require("assert");
 var request = require("supertest");
-describe('Array', function () {
+describe('Server test', function () {
 	
 	// check node env
 	console.log("NODE_ENV is: " + process.env.NODE_ENV);	
-	if (process.env.NODE_ENV !== "test") {
-		console.log("Ok, this is not test")
-	} else {
-		console.log("HAEEEYY, THIS IS TEST");
-	}
 	
 	// run server
 	beforeEach(function () {

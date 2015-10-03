f8b4b7ac3a98f57b60cb47af7926ba5145d333a5
bkazic <blaz.kazic@ijs.si>
2015-10-03 13:35:58 +0200

    Testing NODE_ENV is test

--- test/server-test.js
+++ test/server-test.js
@@ -1,5 +1,14 @@
 ﻿var assert = require("assert");
 describe('Array', function () {
+	
+console.log("NODE_ENV is: " + process.env.NODE_ENV);	
+	
+if (process.env.NODE_ENV !== "test") {
+	console.log("Ok, this is not test")
+} else {
+	console.log("HAEEEYY, THIS IS TEST");
+}
+	
     describe('#indexOf()', function () {
         it('should return -1 when the value is not present', function () {
             assert.equal(-1, [1, 2, 3].indexOf(5));
